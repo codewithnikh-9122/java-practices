62b1d8b5796cd56854675eeccb4d42102ffe83e5
codewithnikh-9122 <nikhilsahfbg789@gmail.com>
2026-07-19 02:52:30 +0530

    new added

---
+++ src/loop/switchcase1.java
@@ -0,0 +1,50 @@
+//write a java program in java to print days along day number
+
+package loop;
+import java.util.Scanner;
+public class switchcase1 {
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter day name");
+		String dayname= scan.nextLine().toLowerCase();
+		
+		int day=0;
+		switch(dayname) {
+		case "Sunday":
+			day =1;
+			System.out.println("day number"+day);
+			break;
+		case "monday":
+			day=2;
+			System.out.println("day number"+day);
+			break;
+		case "tuesday":
+			day=3;
+			System.out.println("day number"+day);
+			break;
+		case "wednesday":
+			day=4;
+			System.out.println("day number"+day);
+			break;
+		case "thursday":
+			day=5;
+			System.out.println("day number"+day);
+			break;
+		case "friday":
+			day=6;
+			System.out.println("day number"+day);
+			break;
+		case "saturday":
+			day=7;
+			System.out.println("day number"+day);
+			break;
+			
+			default:
+				System.out.println("no match found");
+		} 
+		scan.close();
+
+	}
+
+}
