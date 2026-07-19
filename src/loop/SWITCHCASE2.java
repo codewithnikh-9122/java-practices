62f060f922551700e1cd15182ec7a4071e1b6046
codewithnikh-9122 <nikhilsahfbg789@gmail.com>
2026-07-19 23:35:18 +0530

    new added

---
+++ src/loop/SWITCHCASE2.java
@@ -0,0 +1,71 @@
+//write a java program to define menu driven calculator using switch statement
+
+
+
+package loop;
+import java.util.Scanner;
+public class SWITCHCASE2 {
+
+	public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	System.out.println("enter the 1st number");
+	int num=scan.nextInt();
+	System.out.println("enter the 2nd number");
+	int num2=scan.nextInt();
+	System.out.println("Enter the symbol from(+, -, *, /, %):");
+	char symbol = scan.next().charAt(0);
+	
+	int result =0;
+	switch (symbol) {
+	case'+':
+		result=num+num2;
+		System.out.println("sum of "+num +" and "+num2 +" is = "+result);
+		break;
+		
+	case '-':
+		if(num>num2) {
+			result=num-num2;
+			System.out.println("subtraction of "+num +" and "+num2 +" is = "+result);
+		}else {
+			result=num2-num;
+			System.out.println("subtraction of "+num2+" and "+num+" is="+result );
+			break;
+		}
+	case '*':
+		result=num*num2;
+		System.out.println("multiplication of "+num+" and "+num2+" :"+result);
+		break;
+		
+	case '/':
+		if(num>num2) {
+			result=num/num2;
+			System.out.println("Quotient of "+num+" and "+num2+" :"+result);
+			
+		}else{
+			result=num2/num;
+			System.out.println("Quotient of "+num2+" and "+num+" :"+result);
+		break;
+		}
+	case '%':
+		if(num>num2) {
+			result=num%num2;
+			System.out.println("remainder of "+num+" and "+num2+" :"+result);
+		}else {
+			result=num2%num;
+			System.out.println("remainder of "+num2+" and "+num+" :"+result);
+		}
+		break;
+	default:
+		System.out.println("invalid operations");
+	}
+	
+		
+	
+	
+	
+	
+	scan.close();
+
+	}
+
+}
---
+++ src/loop/switchcase3.java
@@ -0,0 +1,67 @@
+//write a java program to check weather the character is vowel or consonent. 
+
+
+package loop;
+import java.util.Scanner;
+public class switchcase3 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the character");
+		char ch=scan.next().toLowerCase().charAt(0);
+		if(Character.isLetter(ch)) {
+			switch(ch) {
+			
+			
+			
+			/*case 'a':
+				System.out.println(ch+ " is a vowel");
+				break;
+				
+			case 'e':
+				System.out.println(ch+ "is vowel");
+				break;
+			case 'i':
+				System.out.println(ch+ "is vowel");
+				break;
+			case 'o':
+				System.out.println(ch+ "is vowel");
+				break;
+			case 'u':
+				System.out.println(ch+ "is vowel");
+				break;
+				default:
+					System.out.println(ch+" is consonent");
+					break;
+		
+			}
+			}else {
+				System.out.println("vowel and consonent check is not possible");
+				
+		}
+		*/
+		
+			//***fall on through condition***//
+			
+			case'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+		
+		System.out.println(ch+ ":is a vowel");
+		break;
+		default:
+			System.out.println(ch+" :is consonent");
+			}
+		}else {
+			System.out.println("vowel and consonent check is not possible");
+			}
+		
+		
+		scan.close();
+		
+
+	}
+
+}
